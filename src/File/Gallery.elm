e79dbe91141e0c5c6ff515660d1f46f604fb1df9
Eddy Lane <naedin@gmail.com>
2018-10-21 22:26:09 +0100

    Gallery WIP

--- src/File/Gallery.elm
+++ src/File/Gallery.elm
@@ -1,26 +1,127 @@
 module File.Gallery exposing
     ( Config
+    , PortCmdMsg
+    , State
     , UploadState
     , config
     , configCancelUploadMsg
     , configContentTypeFn
     , configIdFn
     , configNameFn
+    , configPorts
+    , configSetState
     , configThumbnailSrcFn
+    , getBoundingClientRects
+    , init
+    , subscriptions
     , view
     )
 
 import Css exposing (..)
 import Css.Media
 import Css.Transitions exposing (easeInOut, transition)
+import Dict exposing (Dict)
 import FeatherIcons
 import File.Data.Base64Encoded as Base64Encoded
 import File.Data.UploadId as UploadId exposing (UploadId)
 import File.Upload as Upload exposing (UploadingFile)
 import Html
 import Html.Styled exposing (..)
-import Html.Styled.Attributes exposing (class, css, href, src)
+import Html.Styled.Attributes exposing (class, css, href, id, src)
 import Html.Styled.Events exposing (onClick)
+import Json.Decode as Decode exposing (Decoder)
+import Json.Encode as Encode
+
+
+
+---- PORTS ----
+
+
+type alias PortCmdMsg =
+    { message : Encode.Value
+    , data : Encode.Value
+    }
+
+
+type alias PortSubMsg =
+    { data : Result String Encode.Value
+    , message : String
+    }
+
+
+type alias Ports msg =
+    { cmd : PortCmdMsg -> Cmd msg
+    , sub : (PortCmdMsg -> msg) -> Sub msg
+    }
+
+
+getBoundingClientRects : Config file msg -> List file -> Upload.State -> Cmd msg
+getBoundingClientRects (Config confRec) uploaded uploading =
+    confRec.ports.cmd
+        { message = Encode.string "get-bounding-client-rects"
+        , data =
+            uploading
+                |> combineUploadedAndUploading uploaded
+                |> List.map (fileId confRec)
+                |> Encode.list Encode.string
+        }
+
+
+decodePortMsg : PortCmdMsg -> Maybe PortSubMsg
+decodePortMsg { message, data } =
+    Result.toMaybe <|
+        Result.map
+            (PortSubMsg <| decodePortMsgData data)
+            (Decode.decodeValue Decode.string message)
+
+
+decodePortMsgData : Encode.Value -> Result String Encode.Value
+decodePortMsgData data =
+    data
+        |> Decode.decodeValue (Decode.field "error" Decode.string |> Decode.andThen (Err >> Decode.succeed))
+        |> Result.withDefault (Ok data)
+
+
+
+---- MODEL ----
+
+
+type State
+    = State StateRec
+
+
+init : State
+init =
+    State { current = Dict.empty }
+
+
+type alias StateRec =
+    { current : Dict String Rectangle }
+
+
+type alias Rectangle =
+    { bottom : Float
+    , height : Float
+    , left : Float
+    , right : Float
+    , top : Float
+    , width : Float
+    , x : Float
+    , y : Float
+    }
+
+
+rectDecoder : Decoder Rectangle
+rectDecoder =
+    Decode.map8 Rectangle
+        (Decode.field "bottom" Decode.float)
+        (Decode.field "height" Decode.float)
+        (Decode.field "left" Decode.float)
+        (Decode.field "right" Decode.float)
+        (Decode.field "top" Decode.float)
+        (Decode.field "width" Decode.float)
+        (Decode.field "x" Decode.float)
+        (Decode.field "y" Decode.float)
 
 
 type UploadState file
@@ -40,6 +141,9 @@ type alias ConfigRec file msg =
     , contentTypeFn : file -> String
     , cancelUploadMsg : UploadId -> msg
     , thumbnailSrcFn : file -> String
+    , ports : Ports msg
+    , noOpMsg : msg
+    , setStateMsg : State -> msg
     }
 
 
@@ -56,7 +160,28 @@ config noOpMsg =
         , contentTypeFn = always "-"
         , cancelUploadMsg = always noOpMsg
         , thumbnailSrcFn = always ""
+        , ports =
+            { cmd = always Cmd.none
+            , sub = always Sub.none
+            }
+        , noOpMsg = noOpMsg
+        , setStateMsg = always noOpMsg
         }
+
+
+configPorts : { cmd : PortCmdMsg -> Cmd msg, sub : (PortCmdMsg -> msg) -> Sub msg } -> Config file msg -> Config file msg
+configPorts ports (Config configRec) =
+    Config <|
+        { configRec | ports = ports }
+
+
+{-| Configure a msg that will update the state of this component. Often you will be forced to fill in the blanks when
+you use this library, but sometimes we'll do the leg work for you.
+-}
+configSetState : (State -> msg) -> Config file msg -> Config file msg
+configSetState msg (Config configRec) =
+    Config <|
+        { configRec | setStateMsg = msg }
 
 
 {-| Configure the way in which we can retrieve a unique id (as a string) from a user defined uploaded file
@@ -102,8 +227,8 @@ configCancelUploadMsg cancelUploadMsg (Config configRec) =
         { configRec | cancelUploadMsg = cancelUploadMsg }
 
 
-view : Maybe (Html msg) -> List file -> Upload.State -> Config file msg -> Html msg
-view maybeDropZone uploaded uploading (Config configRec) =
+view : Maybe (Html msg) -> State -> List file -> Upload.State -> Config file msg -> Html msg
+view maybeDropZone (State stateRec) uploaded uploading (Config configRec) =
     let
         dropZone =
             maybeDropZone
@@ -114,9 +239,54 @@ view maybeDropZone uploaded uploading (Config configRec) =
                             [ div [ css [ viewGalleryItemStyle ] ] [ html ] ]
                     )
                 |> Maybe.withDefault (text "")
+    in
+    div
+        [ css
+            [ maxWidth (px 980)
+            , margin2 (px 0) auto
+            ]
+        ]
+        [ div
+            [ id "gallery-visible"
+            , css [ display block ]
+            ]
+            [ viewGallery dropZone uploaded uploading configRec ]
+        , div
+            [ id "gallery-hidden"
+            , css [ display block, position absolute, width (pct 100), height (pct 100) ]
+            ]
+            [ viewHiddenGallery dropZone stateRec ]
+        ]
 
+
+viewGallery : Html msg -> List file -> Upload.State -> ConfigRec file msg -> Html msg
+viewGallery dropZone uploaded uploading configRec =
+    let
         galleryItems =
             viewItems uploaded uploading configRec
+    in
+    div [] (dropZone :: galleryItems)
+
+
+viewHiddenGallery : Html msg -> StateRec -> Html msg
+viewHiddenGallery dropZone stateRec =
+    let
+        galleryItems =
+            stateRec.current
+                |> Dict.values
+                |> List.map
+                    (\c ->
+                        div
+                            [ css
+                                [ viewGalleryItemContainerStyle
+                                , border3 (px 1) dashed (hex "000")
+                                ]
+                            ]
+                            [ div
+                                [ css [ viewGalleryItemStyle ] ]
+                                [ text "hi" ]
+                            ]
+                    )
     in
     div [] (dropZone :: galleryItems)
 
@@ -137,6 +307,14 @@ viewItems uploaded uploading configRec =
         |> List.map (viewItem configRec)
 
 
+viewItem : ConfigRec file msg -> UploadState file -> Html msg
+viewItem configRec upload =
+    viewGalleryItemContainer (fileId configRec upload)
+        [ viewItemThumbnail configRec upload
+        , viewItemUploadOverlay configRec upload
+        ]
+
+
 combineUploadedAndUploading : List file -> Upload.State -> List (UploadState file)
 combineUploadedAndUploading files uploadState =
     List.concat
@@ -148,9 +326,13 @@ combineUploadedAndUploading files uploadState =
         ]
 
 
-viewGalleryItemContainer : List (Html msg) -> Html msg
-viewGalleryItemContainer inner =
-    div [ css [ viewGalleryItemContainerStyle ] ] inner
+viewGalleryItemContainer : String -> List (Html msg) -> Html msg
+viewGalleryItemContainer elementId inner =
+    div
+        [ id elementId
+        , css [ viewGalleryItemContainerStyle ]
+        ]
+        inner
 
 
 viewGalleryItemContainerStyle : Style
@@ -197,14 +379,6 @@ viewGalleryItemStyle =
         , top (px 0)
         , width (pct 100)
         , height (pct 100)
-        ]
-
-
-viewItem : ConfigRec file msg -> UploadState file -> Html msg
-viewItem configRec upload =
-    viewGalleryItemContainer
-        [ viewItemThumbnail configRec upload
-        , viewItemUploadOverlay configRec upload
         ]
 
 
@@ -288,6 +462,16 @@ thumbnailSrc configRec uploadState =
             Nothing
 
 
+fileId : ConfigRec file msg -> UploadState file -> String
+fileId configRec uploadState =
+    case uploadState of
+        Uploading _ file ->
+            Upload.fileFilename file
+
+        Uploaded file ->
+            configRec.idFn file
+
+
 isImage : ConfigRec file msg -> UploadState file -> Bool
 isImage configRec uploadState =
     case uploadState of
@@ -296,3 +480,61 @@ isImage configRec uploadState =
 
         Uploading _ file ->
             Upload.fileIsImage file
+
+
+
+-- SUBSCRIPTIONS ----
+
+
+{-| Subscriptions needed for the Gallery
+-}
+subscriptions : Config file msg -> State -> Sub msg
+subscriptions ((Config confRec) as conf) (State stateRec) =
+    confRec.ports.sub
+        (decodePortMsg
+            >> Maybe.map (dispatchSub confRec stateRec)
+            >> Maybe.withDefault confRec.noOpMsg
+        )
+
+
+dispatchSub : ConfigRec file msg -> StateRec -> PortSubMsg -> msg
+dispatchSub confRec stateRec portsMsg =
+    case portsMsg.message of
+        "get-bounding-client-rects" ->
+            subBoundedRects confRec stateRec portsMsg
+
+        _ ->
+            confRec.noOpMsg
+
+
+type alias BoundedRecSubResponse =
+    { inputId : String
+    , rect : Rectangle
+    }
+
+
+subBoundedRects : ConfigRec file msg -> StateRec -> PortSubMsg -> msg
+subBoundedRects confRec stateRec portsMsg =
+    case portsMsg.data of
+        Ok data ->
+            data
+                |> Decode.decodeValue
+                    (Decode.list
+                        (Decode.map2 BoundedRecSubResponse
+                            (Decode.field "inputId" Decode.string)
+                            (Decode.field "rect" rectDecoder)
+                        )
+                    )
+                |> Result.andThen
+                    (\resultList ->
+                        let
+                            current =
+                                resultList
+                                    |> List.foldl (\{ inputId, rect } acc -> Dict.insert inputId rect acc) stateRec.current
+                        in
+                        Ok (confRec.setStateMsg (State { stateRec | current = current }))
+                    )
+                |> Result.withDefault confRec.noOpMsg
+
+        Err err ->
+            confRec.noOpMsg
